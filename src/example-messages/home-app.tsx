529a1c7ba559a030cf2d41c47760befe34982e9c
edvicaty <edvicaty@hotmail.com>
2020-10-22 18:27:41 -0500

    user:read with user permitions updated from slack app

--- src/example-messages/home-app.tsx
+++ src/example-messages/home-app.tsx
@@ -1,32 +1,35 @@
 import { Actions, Button, Home, PheliaHomeProps, Section, Text } from "phelia";
 import React from "react";
 import { MyModal } from "./modal-example";
-
-export function HomeApp({ useState, useModal }: PheliaHomeProps) {
+export function HomeApp({ useState, useModal, user }: PheliaHomeProps) {
   const [counter, setCounter] = useState("counter", 0);
-  const [loaded, setLoaded] = useState("loaded", 0);
+  const [notifications, setNotifications] = useState("notifications", []);
   const [form, setForm] = useState("form");
-  const [updated, setUpdated] = useState("updated", false);
-  const [user, setUser] = useState("user");
+  console.log(user);
   const openModal = useModal("modal", MyModal, (event) =>
     setForm(JSON.stringify(event.form, null, 2))
   );
-  console.log(`user from home ------------------`, user);
 
   return (
     <Home
-      onLoad={() => setLoaded(loaded + 1)}
-      // onUpdate={() => setUpdated(true)}
-    >
+      onLoad={async (event) => {
+        // const notifications = await fetchNotifications(event.user);
+        // setNotifications(notifications);
+        console.log(`event from onLoad ------------------`, event);
+      }}>
       <Section>
-        <Text emoji>Hey there {user} :wave:</Text>
-        <Text type="mrkdwn">*Updated:* {String(updated)}</Text>
+        <Text emoji>Hey there {`user.username`} :wave:</Text>
         <Text type="mrkdwn">*Counter:* {counter}</Text>
-        <Text type="mrkdwn">*Loaded:* {loaded}</Text>
+        <Text type="mrkdwn">*Notifications:* {notifications.length}</Text>
       </Section>
 
       <Actions>
-        <Button action="counter" onClick={() => setCounter(counter + 1)}>
+        <Button
+          action="counter"
+          onClick={(e) => {
+            console.log(`event -------------`, e);
+            setCounter(counter + 1);
+          }}>
           Click me
         </Button>
 
@@ -43,3 +46,43 @@ export function HomeApp({ useState, useModal }: PheliaHomeProps) {
     </Home>
   );
 }
+// export function HomeApp({ useState, useModal, user }: PheliaHomeProps) {
+//   const [counter, setCounter] = useState("counter", 0);
+//   const [loaded, setLoaded] = useState("loaded", 0);
+//   const [form, setForm] = useState("form");
+//   const [updated, setUpdated] = useState("updated", false);
+
+//   const openModal = useModal("modal", MyModal, (event) =>
+//     setForm(JSON.stringify(event.form, null, 2))
+//   );
+
+//   return (
+//     <Home
+//       onLoad={() => setLoaded(loaded + 1)}
+//       // onUpdate={() => setUpdated(true)}
+//     >
+//       <Section>
+//         <Text emoji>Hey there {`user.username`} :wave:</Text>
+//         <Text type="mrkdwn">*Updated:* {String(updated)}</Text>
+//         <Text type="mrkdwn">*Counter:* {counter}</Text>
+//         <Text type="mrkdwn">*Loaded:* {loaded}</Text>
+//       </Section>
+
+//       <Actions>
+//         <Button action="counter" onClick={() => setCounter(counter + 1)}>
+//           Click me
+//         </Button>
+
+//         <Button action="modal" onClick={() => openModal()}>
+//           Open a Modal
+//         </Button>
+//       </Actions>
+
+//       {form && (
+//         <Section>
+//           <Text type="mrkdwn">{"```\n" + form + "\n```"}</Text>
+//         </Section>
+//       )}
+//     </Home>
+//   );
+// }
