d85bee3f6922e47d3c72527cdd72073450e3ea96
edvicaty <edvicaty@hotmail.com>
2020-10-26 16:17:38 -0600

    modified action attribute to test it on registration-modal

--- src/registration-modal.tsx
+++ src/registration-modal.tsx
@@ -2,14 +2,13 @@ import React from "react";
 
 import { Actions, Button, Modal, PheliaModalProps } from "phelia";
 
-export function RegistrationModal({ useState, props }: PheliaModalProps) {
-  //   const [showForm, setShowForm] = useState("showForm", false);
+export function RegistrationModal({ props }: PheliaModalProps) {
   return (
-    <Modal title={`A fancy pants modal`} submit="cancel">
+    <Modal title={`Register ClickUp account`} submit="cancel">
       <Actions>
         <Button
           url="https://app.clickup.com/api?client_id=RDX22JJQSQWL2RMFXCTLGDOQ39XSN04V&redirect_uri=https://phelia-test-slack.herokuapp.com/auth"
-          action="showForm"
+          action="openURLAuth"
           onClick={() => {}}>
           {`Bind ClickUp account ${props.name}`}
         </Button>
