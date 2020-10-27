e62b214b1237250a07bd7cad16327a4f23362475
edvicaty <edvicaty@hotmail.com>
2020-10-27 16:28:38 -0600

    getting tasks clickUP test

--- src/example-messages/multi-users-select-menu.tsx
+++ src/example-messages/multi-users-select-menu.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import User from "../models/User";
+import axios from "axios";
 
 import {
   Button,
@@ -11,12 +12,31 @@ import {
   PheliaMessageProps,
   Section,
   Text,
+  DatePicker,
 } from "phelia";
 
-//TODO: 2 fetch users clickUP IDs from slack ID's
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+const today = new Date().toISOString().split("T")[0];
+let updatedDate: any = null;
+
 export function MultiUsersSelectMenuModal() {
   return (
     <Modal title="Users multi select menu" submit="Submit">
+      <Section
+        text={`Select a day`}
+        accessory={
+          <DatePicker
+            initialDate={today}
+            onSelect={async ({ user, date }) => {
+              await delay(2000);
+              //2020-10-26 date format === date
+              //timeStamp is for sending request to clickUP API
+              updatedDate = new Date(date).getTime() / 1000;
+            }}
+            action="date"
+          />
+        }
+      />
       <Input label="Select menu">
         <MultiSelectMenu
           type="users"
@@ -41,15 +61,37 @@ export function MultiUsersSelectMenuExample({
     async (event) => {
       user = event.user;
       form = event.form;
-      //form.selection to get users IDs is an array of slack IDs
       const query = form.selection.map((id: any) => {
         return { slackID: id };
       });
-      console.log(`query -------------------`, query);
-      const users = await User.find({ $or: query });
-      console.log(`users ----------------------`, users);
+      const usersArr = await User.find({ $or: query });
+      // console.log(`users ----------------------`, usersArr);
+      const usersString = usersArr.toString();
+      await getFilteredTasks(usersString);
     }
   );
+
+  async function getFilteredTasks(users: string) {
+    //TODO: page pending
+    //TODO: date pending check for date Updated
+    const teamID = 8509000;
+    const page = 0;
+    const oneDay = 1588671070;
+    const dateLt = updatedDate + oneDay;
+
+    const tasks = await axios.get(
+      `https://api.clickup.com/api/v2/team/${teamID}/task?page=${page}&date_updated_gt=${updatedDate}&date_updated_lt=${dateLt}&assignees[]=${users}`
+    );
+
+    console.log(
+      `tasks --------------`,
+      updatedDate,
+      oneDay,
+      dateLt,
+      tasks,
+      users
+    );
+  }
 
   return (
     <Message text="Get tasks">
