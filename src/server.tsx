a1669796697ad2f303c6fafca59901832e5c5627
edvicaty <edvicaty@hotmail.com>
2020-11-04 15:41:43 -0600

    test 5

--- src/server.tsx
+++ src/server.tsx
@@ -123,7 +123,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: "octavia-bot-test",
     clientEmail: "octavia-bot-test@octavia-bot-test.iam.gserviceaccount.com",
-    privateKey: process.env.PRIVATE_KEY_GCP,
+    privateKey: process.env.PRIVATE_KEY_GCP.replace(/\\n/g, "\n"),
   }),
   databaseURL: "https://test-firestore-b9cbd.firebaseio.com",
 });
