8a4e661250e7d65ed1e0a3c57965cbd8f2786f7e
edvicaty <edvicaty@hotmail.com>
2020-11-15 11:16:52 -0600

    testing octavia code graphql

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,22 +1,22 @@
-import express from "express"
-import { GetTasksCurrentUser } from "../components/GetTasksCurrentUser"
-import { GetTasks } from "../components/GetTasks"
-import { TextMessage } from "../components/TextMessage"
-import db from "../config/firestore"
-import client from "../config/phelia"
-import getData from "../graphQL/fetchData"
+import express from "express";
+import { GetTasksCurrentUser } from "../components/GetTasksCurrentUser";
+import { GetTasks } from "../components/GetTasks";
+import { TextMessage } from "../components/TextMessage";
+import db from "../config/firestore";
+import client from "../config/phelia";
+import getData from "../graphQL/fetchData";
 
-const router = express.Router()
+const router = express.Router();
 
 //get tasks admin : /get-tasks command
 router.post("/get-tasks-admin", async function (req, res) {
-  await res.sendStatus(200)
+  await res.sendStatus(200);
 
-  const { user_id } = await req.body
+  const { user_id } = await req.body;
 
   const slackIdVar = {
     slackId: user_id,
-  }
+  };
 
   const getUserBySlackIdQuery = `
   query($slackId: String!) {
@@ -43,25 +43,25 @@ router.post("/get-tasks-admin", async function (req, res) {
       }
     }
   }
-  `
+  `;
 
-  const user = await getData(getUserBySlackIdQuery, slackIdVar)
+  const user = await getData(getUserBySlackIdQuery, slackIdVar);
 
   if (user.filtered[0].id && user.filtered[0].services.clickUp.isAdmin) {
-    client.postMessage(GetTasks, `${user_id}`)
+    client.postMessage(GetTasks, `${user_id}`);
   } else {
-    const message = `You need to have ADMIN status to be able to check the tasks of all users. Try /setAdmin [token] to set yourself as an ADMIN. \n If you want to check your own tasks try /get-my-tasks command`
-    client.postMessage(TextMessage, `${user_id}`, { message })
+    const message = `You need to have ADMIN status to be able to check the tasks of all users. Try /setAdmin [token] to set yourself as an ADMIN. \n If you want to check your own tasks try /get-my-tasks command`;
+    client.postMessage(TextMessage, `${user_id}`, { message });
   }
-})
+});
 
 //get tasks current user : /get-my-tasks command
 router.post("/get-tasks-user", async function (req, res) {
-  await res.sendStatus(200)
+  await res.sendStatus(200);
 
-  const { user_id } = await req.body
+  const { user_id } = await req.body;
 
-  client.postMessage(GetTasksCurrentUser, `${user_id}`)
-})
+  client.postMessage(GetTasksCurrentUser, `${user_id}`);
+});
 
-export default router
+export default router;
